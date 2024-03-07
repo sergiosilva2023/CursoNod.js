9b5828b6dc53accd66d40224b9d3cb2fbdd158e8
Sérgio Silva <sergio.web100@gmail.com>
2024-03-07 11:58:31 +0000

    mais um update

--- Test/calc.js
+++ Test/calc.js
@@ -1,15 +1,9 @@
-function soma(){
-    return a+b
-}
+var SomaFunc = require("./somar")
+var SubFunc = require("./sub")
+var DivFunc = require("./div")
+var MultiFunc = require("./multi")
 
-function sub(){
-    return a-b
-}
-
-function multi(){
-    return a*b
-}
-
-function div(){
-    return a/b
-}
+console.log(SomaFunc(5,5))
+console.log(SubFunc(5,5))
+console.log(DivFunc(5,5))
+console.log(MultiFunc(5,5))
---
+++ Test/multi.js
@@ -0,0 +1,4 @@
+var multi = function (a,b){
+    return a*b
+}
+module.exports = multi
